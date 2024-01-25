ac716cfb0b82c9ee14b0c19d70b84090bc0d8041
Sarim <devsarim.work@gmail.com>
2024-01-25 13:53:04 +0500

    render settings and pixelate menus

--- src/App/init.lua
+++ src/App/init.lua
@@ -1,9 +1,23 @@
 local React = require(script.Parent.Libs.React)
 
 local Navigation = require(script.Navigation)
+local Pixelate = require(script.Pixelate)
+local Settings = require(script.Settings)
+local settingCreator = require(script.Settings.settingCreator)
 
 return function()
 	local page, setPage = React.createBinding("Pixelate")
+
+	local settingsList = {
+		keepOldModel = settingCreator.boolean(
+			false,
+			"Keep old model",
+			"Keep or destroy selected model",
+			"Keep old model"
+		),
+
+		scanResolution = settingCreator.number(2, "Scan resolution", "Quality of scan", "2"),
+	}
 
 	return React.createElement("Frame", {
 		Size = UDim2.fromScale(1, 1),
@@ -11,9 +25,21 @@ return function()
 		BackgroundColor3 = Color3.fromRGB(46, 46, 46),
 		BorderSizePixel = 0,
 	}, {
-		Navigaton = React.createElement(Navigation, {
+		Navigation = React.createElement(Navigation, {
 			page = page,
 			setPage = setPage,
+		}),
+
+		Pixelate = React.createElement(Pixelate, {
+			page = page,
+
+			settingsList = settingsList,
+		}),
+
+		Settings = React.createElement(Settings, {
+			page = page,
+
+			settingsList = settingsList,
 		}),
 	})
 end
